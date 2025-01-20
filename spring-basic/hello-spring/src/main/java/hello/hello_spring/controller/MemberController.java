9f0d21b7a259c787e348e794d484f4e8690bfbfa
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-01-20 09:35:48 +0900

    feat: 회원 관리 기능 구현
    - 회원 등록 기능 구현 (MVC 패턴)
    - 회원 목록 조회 기능 구현
    - Form 객체를 활용한 데이터 바인딩
    - Thymeleaf를 활용한 화면 구현

--- spring-basic/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ spring-basic/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // MemberController 객체 생성해서 스프링에 넣어두고 스프링 컨테이너가 관리함.
 public class MemberController {
@@ -36,13 +39,22 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
-    @PostMapping("/members/new")
-    public String create(MemberForm form) {
+    @PostMapping("/members/new")            // HTML form 데이터를 MemberForm 객체로 변환: name 속성값과 MemberForm 필드명이 일치하면(여기선 name) 스프링이 자동으로 데이터 채워줌
+    public String create(MemberForm form) { // 여기의 MemberForm으로 자동 바인딩됨
         Member member = new Member();
-        member.setName(form.getName());
+        member.setName(form.getName()); // 새 Member 객체에 form 데이터 가져옴
 
-        memberService.join(member);
+//        System.out.println("member.getName() = " + member.getName()); // 잘 전달됐는지 확인해봄
 
-        return "redirect:/";
+        memberService.join(member); // 회원가입 서비스 호출
+
+        return "redirect:/"; // 홈 화면으로 리다이렉트
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) { // Medel: 화면에 데이터를 전달하기 위한 객체 -> 컨트롤러에서 화면(view)으로 데이터 전달해야 할 때 씀 (특히 동적으로 변하는 데이터 화면에 표시할 때)
+        List<Member> members = memberService.findMembers(); // 모든 회원 조회하는 서비스 호출
+        model.addAttribute("members", members); // "members"라는 이름으로 회원 목록을 모델에 추가
+        return "members/memberList";
     }
 }
--- spring-basic/hello-spring/src/main/java/hello/hello_spring/controller/MemberForm.java
+++ spring-basic/hello-spring/src/main/java/hello/hello_spring/controller/MemberForm.java
@@ -1,5 +1,8 @@
 package hello.hello_spring.controller;
 
+// HTML form에서 넘어오는 데이터를 임시로 담아두는 객체가 필요함
+// 나중에 추가 정보(이메일, 비밀번호 등)가 필요할 때 MemberForm만 수정하면 됨
+// 웹 계층에서만 사용되는 객첼, 화면에서 넘어오는 데이터 임시로 담는 용도이므로 Controller들과 같은 패키지에 위치
 public class MemberForm {
     private String name;
 
