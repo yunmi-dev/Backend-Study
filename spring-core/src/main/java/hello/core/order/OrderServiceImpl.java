b1c213538e71a9b3cf61452ec207614531017752
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-02-11 23:36:27 +0900

    refactor: OrderServiceImpl 인터페이스 의존성 개선
    - 구체 클래스 의존성 제거
    - DIP를 준수하도록 DiscountPolicy 인터페이스에만 의존하도록 변경
    - 구현체 주입 필요성 주석으로 명시

--- spring-core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -11,10 +11,9 @@ public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    // 문제점: 이 수정은 OCP, DIP 등의 객제 지향 원칙를 어기고 있음을 보여줌
-    // 인터페이스인 DiscountPolicy에만 의존하게 하려 했으나, 실제로는 구체 클래스인 FixDiscountPolicy에도 의존하고 있음 -> 추상과 구체를 전부 의존하여 DIP 위반
-    // 정책 변경으로 FixDiscountPolicy에서 RateDiscountPolicy로 변경하는 순간 OrderServiceImpl의 소스코드도 함께 변경해야 함 -> OCP 위반
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // OCP, DIP 위반 문제
+    private final DiscountPolicy discountPolicy;    // 구현체 없이 인터페이스에만 의존하도록 하니 NullPointerException 발생
+                                                    // 누군가가 클라이언트인 OrderServiceImpl에 DiscountPolicy 구현 객체를 대신 생성 & 주입해줘야 함
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
