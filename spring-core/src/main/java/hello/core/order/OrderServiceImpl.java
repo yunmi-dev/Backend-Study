09dd9e0c2194ac761c9200f2b9c6d2f3ac8d155e
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-02-11 23:32:58 +0900

    fix: OrderServiceImpl 할인 정책 변경 및 문제점 주석 추가
    - FixDiscountPolicy에서 RateDiscountPolicy로 변경
    - OCP, DIP 위반 사항 코드 주석으로 명시

--- spring-core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,11 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // 문제점: 이 수정은 OCP, DIP 등의 객제 지향 원칙를 어기고 있음을 보여줌
+    // 인터페이스인 DiscountPolicy에만 의존하게 하려 했으나, 실제로는 구체 클래스인 FixDiscountPolicy에도 의존하고 있음 -> 추상과 구체를 전부 의존하여 DIP 위반
+    // 정책 변경으로 FixDiscountPolicy에서 RateDiscountPolicy로 변경하는 순간 OrderServiceImpl의 소스코드도 함께 변경해야 함 -> OCP 위반
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
